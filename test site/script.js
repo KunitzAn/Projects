0d115c1679e81754a0d9335cc2efb47e449c0244
KunitzAn <kunitskaiaanastasia@gmail.com>
2022-09-07 16:00:52 +0300

    site updated

--- test site/script.js
+++ test site/script.js
@@ -112,7 +112,133 @@ function buttonCkeck(id) {
     }   
 }
 
+
+
+
+
 function openForm(id){
     var form = document.getElementById("form-"+id);
-    form.classList.remove("invisible");
+
+    var allForms = document.getElementsByClassName("form");
+    var invisibleForms = document.getElementsByClassName("invisible");
+    
+    if(invisibleForms.length == 2){
+        form.classList.remove("invisible");
+    } else{
+        [].forEach.call(allForms, function (element) {
+            element.classList.add("invisible")
+        });
+        form.classList.remove("invisible");
+    }
+
+}
+
+
+
+
+function chooseStaff(){
+    var workPeriodMin = document.getElementById("work-years-min").value;
+    var workPeriodMax = document.getElementById("work-years-max").value;
+    var ageMin = document.getElementById("age-min").value;
+    var ageMax = document.getElementById("age-max").value;
+    var professionNum = document.getElementById("staffKind").value;
+
+
+    if(ageMin>ageMax){
+        ageMax = ageMin;
+        document.getElementById("work-years-max").value=ageMax;
+    }
+    var ageArr = [];
+    for(let i = ageMin-1; i<= ageMax; i++) {
+        ageArr.push(i);
+    }
+
+    if(workPeriodMin>workPeriodMax){
+        workPeriodMax = workPeriodMin;
+        document.getElementById("work-years-max").value=workPeriodMax;
+    }
+    var workExperienceArr = [];
+    for(let i = workPeriodMin-1; i<= workPeriodMax; i++) {
+        workExperienceArr.push(i);
+    }
+
+
+    /* make all rows invisible */
+
+    var allStaff = document.getElementsByClassName("person");
+
+    [].forEach.call(allStaff, function (element) {
+        element.classList.remove("visible")
+    });
+
+    /* make chosen rows visible */
+    var arrRightProffesion;
+    let age;
+    let workExperience;
+    switch(professionNum) {
+        case '1':
+            arrRightProffesion = document.getElementsByClassName("nurse");
+            age = 30;
+            workExperience = 5;
+            if(workExperienceArr.includes(workExperience) && ageArr.includes(age)){
+                [].forEach.call(arrRightProffesion, function (element) {
+                    element.classList.add("visible")
+                });
+            }
+            break;
+        case '3':
+            arrRightProffesion = document.getElementsByClassName("gardner");
+            age = 50;
+            workExperience = 20;
+            if(workExperienceArr.includes(workExperience) && ageArr.includes(age)){
+                [].forEach.call(arrRightProffesion, function (element) {
+                    element.classList.add("visible")
+                });
+            }
+            break;
+        case '4':
+            arrRightProffesion = document.getElementsByClassName("cleaner");
+            age = 35;
+            workExperience = 7;
+            if(workExperienceArr.includes(workExperience) && ageArr.includes(age)){
+                [].forEach.call(arrRightProffesion, function (element) {
+                    element.classList.add("visible")
+                });
+            }
+            break;
+        case '5':
+            arrRightProffesion = document.getElementsByClassName("groom");
+            age = 60;
+            workExperience = 15;
+            if(workExperienceArr.includes(workExperience) && ageArr.includes(age)){
+                [].forEach.call(arrRightProffesion, function (element) {
+                    element.classList.add("visible")
+                });
+            }
+            break;
+        case '6':
+            arrRightProffesion = document.getElementsByClassName("cook");
+            age = 55;
+            workExperience = 30;
+            if(workExperienceArr.includes(workExperience) && ageArr.includes(age)){
+                [].forEach.call(arrRightProffesion, function (element) {
+                    element.classList.add("visible")
+                });
+            }
+            break;
+
+
+
+        case '2':
+            arrRightProffesion = document.getElementsByClassName("gouvernante");
+            age = 45;
+            workExperience = 15;
+            if(workExperienceArr.includes(workExperience) && ageArr.includes(age)){
+                [].forEach.call(arrRightProffesion, function (element) {
+                    element.classList.add("visible")
+                });
+            }
+            break;
+    }
+
 }
